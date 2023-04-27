b84544353a97a0c1ba058ccb8e9e954095f38270
unknown <quangtaobaongoc@gmail.com>
2023-04-28 00:02:20 +0700

    thêm menu đồ uống

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let foodList = [];
-
+let index_storage = "foodList"
 function renderTable() {
     let tbody = document.getElementById("tbody");
     tbody.innerHTML = "";
@@ -26,14 +26,13 @@ function renderTable() {
 }
 
 function saveToLocalStorage() {
-    localStorage.setItem("foodList", JSON.stringify(foodList));
+    localStorage.setItem(index_storage, JSON.stringify(foodList));
 }
 
-function loadFromLocalStorage() {
-    let foodListJson = localStorage.getItem("foodList");
-    if (foodListJson) {
-        foodList = JSON.parse(foodListJson);
-    }
+function loadFromLocalStorage(key) {
+    let foodListJson = localStorage.getItem(key);
+    foodList = JSON.parse(foodListJson)??[];
+    console.log(foodList,key)
     renderTable();
 }
 
@@ -50,17 +49,49 @@ form.addEventListener("submit", function (event) {
     }
 });
 
-loadFromLocalStorage();
+loadFromLocalStorage("foodList");
 function notify(message,status) {
     $("#random").notify(message, { className:status,  autoHide: false,});
 }
 document.getElementById("random").addEventListener("click", function () {
     if (foodList.length) {
         const randomItem = foodList[Math.floor(Math.random() * foodList.length)];
-        notify(`Hôm nay bạn nên ăn: ${randomItem}`,"info");
+        if(index_storage=="drinkList"){
+            notify(`Hôm nay bạn nên uống: ${randomItem}`,"info");
+        }
+        else {
+            notify(`Hôm nay bạn nên ăn: ${randomItem}`,"info");
+        }
     }
     else {
-        notify(`Chưa có món ăn nào để random`,"danger")
+        notify(`Chưa có món nào để random`,"danger")
     }
 })
 
+
+const menuItems = document.querySelectorAll('nav a');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', function(element) {
+    // Xóa lớp CSS 'active' của tất cả các mục menu
+    menuItems.forEach(item => {
+      item.classList.remove('active');
+    });
+    // Thêm lớp CSS 'active' cho mục được chọn
+    this.classList.add('active');
+
+    id = element.target.getAttribute('data-id')
+    index_storage = id
+    if(id=="drinkList"){
+        document.getElementById("title").innerHTML = "Quản lý đồ uống"
+        document.getElementById("name").placeholder  = "Nhập tên đồ uống"
+        document.getElementById("th").innerHTML  = "Tên đồ uống"
+    }
+    else {
+        document.getElementById("title").innerHTML = "Quản lý đồ ăn" 
+        document.getElementById("name").placeholder  = "Nhập tên món ăn"
+        document.getElementById("th").innerHTML  = "Tên món ăn"
+    }
+    loadFromLocalStorage(id)
+  });
+});
