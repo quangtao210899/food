949bc660a1edee800701c3e0f7dc8e41dcae71dd
Nguyễn Quang Tạo <quangtaobaongoc@gmail.com>
2023-04-27 15:20:57 +0700

    fix

---
+++ script.js
@@ -0,0 +1,63 @@
+let foodList = [];
+
+function renderTable() {
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    for (let i = 0; i < foodList.length; i++) {
+        let tr = document.createElement("tr");
+        let idTd = document.createElement("td");
+        idTd.innerText = i + 1;
+        let nameTd = document.createElement("td");
+        nameTd.innerText = foodList[i];
+        let deleteTd = document.createElement("td");
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Xóa";
+        deleteButton.addEventListener("click", function () {
+            foodList.splice(i, 1);
+            renderTable();
+            saveToLocalStorage();
+        });
+        deleteTd.appendChild(deleteButton);
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(deleteTd);
+        tbody.appendChild(tr);
+    }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("foodList", JSON.stringify(foodList));
+}
+
+function loadFromLocalStorage() {
+    let foodListJson = localStorage.getItem("foodList");
+    if (foodListJson) {
+        foodList = JSON.parse(foodListJson);
+    }
+    renderTable();
+}
+
+let form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let nameInput = document.getElementById("name");
+    let name = nameInput.value.trim();
+    if (name !== "") {
+        foodList.push(name);
+        nameInput.value = "";
+        renderTable();
+        saveToLocalStorage();
+    }
+});
+
+loadFromLocalStorage();
+
+document.getElementById("random").addEventListener("click", function () {
+    if (foodList.length) {
+        const randomItem = foodList[Math.floor(Math.random() * foodList.length)];
+        alert(`Hôm nay bạn nên ăn: ${randomItem}`);
+    }
+    else {
+        alert(`Chưa có món ăn nào để random`);
+    }
+})
